58eb37be4544fe39ec408f6ef9163edb58dd75be
Greg Yerkes <gyerkes@rajant.com>
2024-03-04 22:29:58 -0500

    initial signup and login page

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,17 @@
 const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
-  name: {
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
     type: String,
     required: true,
     unique: true,
