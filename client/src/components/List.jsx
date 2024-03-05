2e6fc55390a0199bbcbd315b01901ddafd94a4d0
DBrush <coldnebraska@gmail.com>
2024-03-04 21:28:06 -0600

    added dynamic rating display

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,16 +1,22 @@
 function List({ recipes }) {
     return (
         <>
-            {recipes.map(recipe => (
+            {recipes.map(recipe => {
+                let rating = ""
+                for (let i = 0; i < recipe.rating; i++) {
+                    rating += "⭐"
+                }
+                
+                return (
                 <div className="search-card" key={recipe._id}>
                     <h1 className="search-title">{recipe.name}</h1>
                     <p>Cook Time: {recipe.cook_time}</p>
                     <div className="search-hover hidden">
-                        <p>Rating: {recipe.rating}</p>
+                        <p>Rating: {rating}</p>
                         <p>Ingredients: {recipe.ingredients}</p>
                     </div>
                 </div>
-            ))}
+            )})}
         </>
     )
 }
