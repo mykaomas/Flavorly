76df0a0d5bf8a2be3eb9fb3a28ac11e3913f9b56
Greg Yerkes <gyerkes@rajant.com>
2024-03-06 19:28:29 -0500

    added auth.js and middleware to server

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   username: {
@@ -10,6 +11,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
   },
   password: {
     type: String,
@@ -25,6 +27,19 @@ const userSchema = new Schema({
     required: false
   }
 })
+
+profileSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+profileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model('User', userSchema)
 
