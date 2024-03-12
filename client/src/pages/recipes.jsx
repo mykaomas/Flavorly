8578792e79b9c3b5f22bedd7c8f63e276e0a9416
YvetteCarbajal <yvettemcj@gmail.com>
2024-03-12 04:19:13 -0500

    recipes.jsx revised

--- client/src/pages/recipes.jsx
+++ client/src/pages/recipes.jsx
@@ -1,48 +1,64 @@
 import React, { useState } from 'react';
 import '../components/Recipes/css/Recipes.css';
-import Header from '../components/header/Header'
+import Header from '../components/header/header'
 import '../components/header/css/Header.css'
 import ChickenNoodleSoup from '../Images/ChickenNoodleSoup.jpg'
+import CreamyGarlicChicken from '../Images/CreamyGarlicChicken.jpg'
+import ChickenStirFry from '../Images/ChickenStirFry.jpg'
+import ShrimpAlfredoPasta from '../Images/ShrimpAlfredoPasta.jpg'
+import  ChickenPotPieSoup from '../Images/ChickenPotPieSoup.jpg'
+import LemonPie from '../Images/LemonPie.jpg'
+import ChickenDumplingSoup from '../Images/ChickenDumplingSoup.webp'
+import Lasagna from '../Images/Lasagna.jpg'
+import Enchiladas from '../Images/Enchiladas.jpg'
 
 function Recipes() {
     const [cards] = useState([
         {
             title: 'Chicken Noodle Soup',
             text: ' is a comforting and classic dish made with chicken, vegetables, noodles, and flavorful broth',
-            Image: ChickenNoodleSoup
+            image: ChickenNoodleSoup
         },
         {
             title: 'Creamy Garlic Chicken',
-            text: 'Creamy Garlic Chicken is a delicious dish featuring tender chicken breasts cooked in a rich and flavorful creamy garlic sauce'
+            text: 'Creamy Garlic Chicken is a delicious dish featuring tender chicken breasts cooked in a rich and flavorful creamy garlic sauce',
+            image: CreamyGarlicChicken
         },
         {
             title: 'Chicken Stir Fry',
-            text: 'Chicken Stir Fry is a vibrant and flavorful dish featuring tender chicken pieces, crisp vegetables, and savory sauce, all quickly cooked together in a hot wok or skillet'
+            text: 'Chicken Stir Fry is a vibrant and flavorful dish featuring tender chicken pieces, crisp vegetables, and savory sauce, all quickly cooked together in a hot wok or skillet',
+            image: ChickenStirFry
             
         },
         { 
             title: 'Shrimp Alfredo Pasta ',
-            text: 'Shrimp Alfredo Pasta is a decadent and indulgent dish featuring succulent shrimp tossed in a creamy Alfredo sauce and served over a bed of perfectly cooked past'
+            text: 'Shrimp Alfredo Pasta is a decadent and indulgent dish featuring succulent shrimp tossed in a creamy Alfredo sauce and served over a bed of perfectly cooked past',
+            image: ShrimpAlfredoPasta
         },
         {
             title: 'Chicken Pot Pie Soup',
-            text: 'Chicken Pot Pie Soup is a comforting and hearty soup that captures all the flavors of a traditional chicken pot pie in a warm and satisfying bowl'
+            text: 'Chicken Pot Pie Soup is a comforting and hearty soup that captures all the flavors of a traditional chicken pot pie in a warm and satisfying bowl',
+            image: ChickenPotPieSoup
         },
         {
             title: 'Lemon Pie ',
-            text: 'Lemon Pie is a delightful dessert that features a tangy and sweet lemon filling nestled in a flaky pie crust'
+            text: 'Lemon pie is a tangy and refreshing dessert made with a buttery crust filled with a smooth lemon filling',
+            image: LemonPie
         },
         {
-            title: 'Chicken and Dumplings',
-            text: 'Chicken and Dumplings is a comforting and classic dish that features tender pieces of chicken, vegetables, and fluffy dumplings simmered in a savory broth'
+            title: 'Chicken & Dumplings Soup',
+            text: 'Chicken and dumplings is a comforting and hearty dish that features tender chunks of chicken simmered in a savory broth with vegetables like carrots, celery, and onions',
+            image: ChickenDumplingSoup
         },
         {
             title: 'Lasagna',
-            text: 'Lasagna is a delicious and hearty dish that consists of layers of wide pasta sheets, rich meat sauce, creamy cheese, and savory seasonings, baked to perfection'
+            text: 'Lasagna is a popular Italian dish that consists of layers of wide, flat pasta sheets, rich tomato sauce, creamy béchamel sauce, flavorful meat (such as ground beef or sausage), and melted cheese, typically mozzarella and Parmesan',
+            image: Lasagna
         },
         {
             title: 'Enchiladas',
-            text: 'Enchiladas are a delicious and flavorful Mexican dish made by filling corn or flour tortillas with a mixture of meat, cheese, beans, or vegetables, then rolling them up and covering them with a savory chili sauce'
+            text: 'Enchiladas are a traditional Mexican dish made by filling corn tortillas with a variety of ingredients such as seasoned meat (like chicken, beef, or pork), beans, cheese, and vegetables',
+            image: Enchiladas
         },
 ]);
 
@@ -57,7 +73,6 @@ return (
                   <div key={i} className="card" style={{ backgroundImage: `url(${card.image})` }}>
                     <p>{card.text}</p>
                     <h3>{card.title}</h3>
-                    <img src="" alt="" />
                   </div>
                     ))}
             </div>
