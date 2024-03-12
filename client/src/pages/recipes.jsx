04ff0d7e48f2d5f295c4a602181d696384e7c4b4
YvetteCarbajal <yvettemcj@gmail.com>
2024-03-11 19:54:50 -0500

    modified recipes

--- client/src/pages/recipes.jsx
+++ client/src/pages/recipes.jsx
@@ -2,45 +2,47 @@ import React, { useState } from 'react';
 import '../components/Recipes/css/Recipes.css';
 import Header from '../components/header/Header'
 import '../components/header/css/Header.css'
+import ChickenNoodleSoup from '../Images/ChickenNoodleSoup.jpg'
 
 function Recipes() {
     const [cards] = useState([
         {
             title: 'Chicken Noodle Soup',
-            text: 'sdfbkeuwbsdkvfb'
+            text: ' is a comforting and classic dish made with chicken, vegetables, noodles, and flavorful broth',
+            Image: ChickenNoodleSoup
         },
         {
             title: 'Creamy Garlic Chicken',
-            text: 'sdfbkeuwbsdkvfb'
+            text: 'Creamy Garlic Chicken is a delicious dish featuring tender chicken breasts cooked in a rich and flavorful creamy garlic sauce'
         },
         {
             title: 'Chicken Stir Fry',
-            text: 'sdfbkeuwbsdkvfb'
+            text: 'Chicken Stir Fry is a vibrant and flavorful dish featuring tender chicken pieces, crisp vegetables, and savory sauce, all quickly cooked together in a hot wok or skillet'
             
         },
         { 
             title: 'Shrimp Alfredo Pasta ',
-            text: 'sdfbkeuwbsdkvfb'
+            text: 'Shrimp Alfredo Pasta is a decadent and indulgent dish featuring succulent shrimp tossed in a creamy Alfredo sauce and served over a bed of perfectly cooked past'
         },
         {
             title: 'Chicken Pot Pie Soup',
-            text: 'sdfbkeuwbsdkvfb'
+            text: 'Chicken Pot Pie Soup is a comforting and hearty soup that captures all the flavors of a traditional chicken pot pie in a warm and satisfying bowl'
         },
         {
             title: 'Lemon Pie ',
-            text: 'sdfbkeuwbsdkvfb'
+            text: 'Lemon Pie is a delightful dessert that features a tangy and sweet lemon filling nestled in a flaky pie crust'
         },
         {
             title: 'Chicken and Dumplings',
-            text: 'sdfbkeuwbsdkvfb'
+            text: 'Chicken and Dumplings is a comforting and classic dish that features tender pieces of chicken, vegetables, and fluffy dumplings simmered in a savory broth'
         },
         {
             title: 'Lasagna',
-            text: 'sdfbkeuwbsdkvfb'
+            text: 'Lasagna is a delicious and hearty dish that consists of layers of wide pasta sheets, rich meat sauce, creamy cheese, and savory seasonings, baked to perfection'
         },
         {
             title: 'Enchiladas',
-            text: 'sdfbkeuwbsdkvfb'
+            text: 'Enchiladas are a delicious and flavorful Mexican dish made by filling corn or flour tortillas with a mixture of meat, cheese, beans, or vegetables, then rolling them up and covering them with a savory chili sauce'
         },
 ]);
 
@@ -52,11 +54,10 @@ return (
              <h1>RECIPES</h1>
              <div className="cards">
                 {cards.map((card, i) => (
-                  <div key={i} className="card">
-                    <img src="client\src\Images\Enchiladas.jpg" alt="card" />
-                    <h3>{card.title}</h3>
+                  <div key={i} className="card" style={{ backgroundImage: `url(${card.image})` }}>
                     <p>{card.text}</p>
-                    <button className='btn'>Search</button>
+                    <h3>{card.title}</h3>
+                    <img src="" alt="" />
                   </div>
                     ))}
             </div>
