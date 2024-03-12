906b38548f0f6f1582ce49a25b00c843818c2b30
Greg Yerkes <gyerkes@rajant.com>
2024-03-11 22:03:17 -0400

    Header links rendering

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
+import Home from './pages/Home.jsx';
 import Signup from './pages/AccountSignup.jsx';
 import Login from './pages/Login.jsx';
 // import Profile from './pages/Vote';
@@ -17,6 +18,10 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        element: <Home />
+      }, {
+
+        path: '/Signup',
         element: <Signup />
       }, {
 
