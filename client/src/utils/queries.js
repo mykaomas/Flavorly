84ca4fdd60b43588663accd62879f7988e33663a
DBrush <coldnebraska@gmail.com>
2024-03-14 10:58:52 -0500

    added query for favorite recipes

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -13,3 +13,18 @@ export const QUERY_RECIPES = gql`
   }
 `
 
+export const QUERY_FAVORITE_RECIPES = gql`
+  query Favorites($userId: ID!) {
+    user(userId: $userId) {
+      favorites {
+        _id
+        ingredients
+        cook_time
+        name
+        rating
+        difficulty
+      }
+    }
+  }
+`
+
